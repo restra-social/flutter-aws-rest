3d9adf389b840d6a77867ff78afabd6556220c49
Fahim Shariar <fahim.shoumik@gmail.com>
2018-10-21 22:09:35 +0600

    flutter format

--- test/flutter_aws_rest_test.dart
+++ test/flutter_aws_rest_test.dart
@@ -1,14 +1,1 @@
-import 'package:test/test.dart';
-import 'package:flutter_aws_rest/flutter_aws_rest.dart';
-
-void main() {
-  final creds = new AwsCredentials("", "");
-  final scope = new AwsScope("ap-south-1", 's3');
-  final signer = new RequestSigner(creds, scope);
-  final awsClient = new AwsClient(signer);
-  final bucketApi = new S3BucketApi("restra", "ap-south-1", awsClient);
-  bucketApi.listBucket().then((ListBucketResult results) {
-    print('Objects in bucket:');
-    results.contents.forEach((content) => print(content.key));
-  });
-}
+void main() {}
---
+++ test/settings.dart
@@ -0,0 +1,6 @@
+library settings;
+
+const AWS_ACCESS_KEY_ID = 'AKIAIV4CQ4KNRV6FXA3Q';
+const AWS_SECRET_ACCESS_KEY = '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';
+const S3_BUCKET = 'restra';
+const AWS_REGION = 'ap-south-1';
